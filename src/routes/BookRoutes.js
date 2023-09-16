6b913d121a7dbc19e0acef57e41176545201a23e
gopalsinghrajput27 <136307083+gopalsinghrajput27@users.noreply.github.com>
2023-09-16 08:51:08 +0530

    Update BookRoutes.js

--- src/routes/BookRoutes.js
+++ src/routes/BookRoutes.js
@@ -12,12 +12,12 @@ const {
 const { requireApiKey } = require("../middlewares/auth");
 const router = express.Router();
 
-router.route("/Books/new").post(createBooks,requireApiKey);
-router.route("/Books").get(getAllBooks,requireApiKey);
+router.route("/Books/new").post(requireApiKey,createBooks);
+router.route("/Books").get(requireApiKey,getAllBooks);
 router
   .route("/Books/:bookName")
-  .get(getbookName)
-  .put(getbookUpdata)
-  .delete(getbookandDelete);
+  .get(requireApiKey,getbookName)
+  .put(requireApiKey,getbookUpdata)
+  .delete(requireApiKey,getbookandDelete);
 
 module.exports = router;
